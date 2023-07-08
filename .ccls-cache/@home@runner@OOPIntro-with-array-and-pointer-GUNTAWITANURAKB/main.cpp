ec6ba6cd8fc081c4e28e6c9a9f545fd5a818670f
GuntawitBen <132637794+GuntawitBen@users.noreply.github.com>
2023-07-08 04:56:11 +0000

    2nd attempt

--- .ccls-cache/@home@runner@OOPIntro-with-array-and-pointer-GUNTAWITANURAKB/main.cpp
+++ .ccls-cache/@home@runner@OOPIntro-with-array-and-pointer-GUNTAWITANURAKB/main.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
 #include <iomanip>
-using namespace std;
-
 #include "student.h"
 
 int main(int argc, char* argv[]) {
- 
   int n = (argc - 1) / 3;
-  student *pt_std = new student[n];
-  //./aout monkey 20 3.5 Art 15 2.5
-  for(int i = 0; i < n; i++, pt_std++){
-    // 3 inputs per n
-    (*pt_std).set_all(argv[i * 3 + 1], atoi(argv[i * 3 + 2]), atof(argv[i * 3 + 3]));
-   }
-  pt_std -= n;
+  student* pt_std = new student[n];
+  student* pt_std_original = pt_std; 
+
+  // Parse command-line arguments and set student attributes
+  for (int i = 0; i < n; i++) {
+    pt_std[i].set_all(argv[i * 3 + 1], atoi(argv[i * 3 + 2]), atof(argv[i * 3 + 3]));
+  }
 
   float max_gpa = 0;
-  for(int i = 0; i < n; i++, pt_std++){
-    if(max_gpa < pt_std->get_gpa()){
-      max_gpa = pt_std->get_gpa();
+  int max_gpa_index = -1;  
+
+  // Find the highest GPA among the students
+  for (int i = 0; i < n; i++) {
+    if (max_gpa < pt_std[i].get_gpa()) {
+      max_gpa = pt_std[i].get_gpa();
+      max_gpa_index = i;
     }
   }
-  pt_std -= n;
-  
-  /*put them in 2 different loops*/
-  for(int i = 0; i < n; i++, pt_std++){  
-    if(pt_std->get_gpa() == max_gpa){
-      pt_std->print_all();
-    }
-  }  
-  pt_std -= n;
-  
-  delete [] pt_std;
-  //name =oat
-  //age =21
-  //score=55
-  //print all info
 
-
+  if (max_gpa_index != -1) {
+    pt_std[max_gpa_index].print_all();
   }
+
+  delete[] pt_std_original; 
+
+  return 0;
+}
